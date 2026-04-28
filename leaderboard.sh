34264ad5e64634a4e64b8bc2b0902c180c7d43d3
BhanuCharan1984 <akagishigeru618@gmail.com>
2026-04-28 22:33:50 +0530

    Changed leaderboard such that it displays for all games separately

--- leaderboard.sh
+++ leaderboard.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
-if [[ $1 == "1" ]]; then
-    game="TicTacToe"
-elif [[ $1 == "2" ]]; then
-    game="Connect_Four"
-else
-    game="Othello"
-fi
-echo "===============   Leaderboard for $game   ==============="
+echo "===============   Leaderboard for TicTacToe   ==============="
 echo "Player               Wins   Losses   Win/Loss Ratio"
-awk -F "," -v game="$game" ' 
-$4 ~ game {Ws[$1]++; Ls[$2]++} 
+echo "----------------------------------------------------"
+awk -F "," -v game="TicTacToe" ' 
+$4==game {Ws[$1]++; Ls[$2]++} 
 END {
     for (player in Ws) {
         if(Ls[player] == 0){
             ratio = "inf";
             printf("%-20s %-6d %-8d %-8s\n", player, Ws[player], Ls[player], ratio)
         } else {
-            ratio = Ws[player] / Ls[player];
+            ratio = Ws[player] / Ls[player]
             printf("%-20s %-6d %-8d %-8.2f\n", player, Ws[player], Ls[player], ratio)
         }
         }
@@ -25,12 +19,67 @@ END {
             printf("%-20s %-6d %-8d %-8.2f\n", player, 0, Ls[player], 0)
         }
     }
-}' history.csv | {
-    if [[ $2 == "1" ]]; then
-        sort -k2,2nr
-    elif [[ $2 == "2" ]]; then
-        sort -k3,3n
-    else
-        sort -k4,4gr
-    fi
-}
+}' history.csv | 
+if [[ $2 == "1" ]]; then
+    sort -k2,2nr
+elif [[ $2 == "2" ]]; then
+    sort -k3,3n
+else
+    sort -k4,4gr
+fi
+echo "===============   Leaderboard for Othello   ==============="
+echo "Player               Wins   Losses   Win/Loss Ratio"
+echo "----------------------------------------------------"
+awk -F "," -v game="Othello" ' 
+$4==game {Ws[$1]++; Ls[$2]++} 
+END {
+    for (player in Ws) {
+        if(Ls[player] == 0){
+            ratio = "inf";
+            printf("%-20s %-6d %-8d %-8s\n", player, Ws[player], Ls[player], ratio)
+        } else {
+            ratio = Ws[player] / Ls[player]
+            printf("%-20s %-6d %-8d %-8.2f\n", player, Ws[player], Ls[player], ratio)
+        }
+        }
+    for (player in Ls) {
+        if (!(player in Ws)) {
+            printf("%-20s %-6d %-8d %-8.2f\n", player, 0, Ls[player], 0)
+        }
+    }
+}' history.csv | 
+if [[ $2 == "1" ]]; then
+    sort -k2,2nr
+elif [[ $2 == "2" ]]; then
+    sort -k3,3n
+else
+    sort -k4,4gr
+fi
+echo "===============   Leaderboard for Connect_Four   ==============="
+echo "Player               Wins   Losses   Win/Loss Ratio"
+echo "----------------------------------------------------"
+awk -F "," -v game="Connect_Four" ' 
+$4==game {Ws[$1]++; Ls[$2]++} 
+END {
+    for (player in Ws) {
+        if(Ls[player] == 0){
+            ratio = "inf";
+            printf("%-20s %-6d %-8d %-8s\n", player, Ws[player], Ls[player], ratio)
+        } else {
+            ratio = Ws[player] / Ls[player]
+            printf("%-20s %-6d %-8d %-8.2f\n", player, Ws[player], Ls[player], ratio)
+        }
+        }
+    for (player in Ls) {
+        if (!(player in Ws)) {
+            printf("%-20s %-6d %-8d %-8.2f\n", player, 0, Ls[player], 0)
+        }
+    }
+}' history.csv | 
+if [[ $2 == "1" ]]; then
+    sort -k2,2nr
+elif [[ $2 == "2" ]]; then
+    sort -k3,3n
+else
+    sort -k4,4gr
+fi
